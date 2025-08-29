5bf03caf6dda0a03751184970ebe2ad20632e4be
TejasJay <tejasjayprakash@gmail.com>
2025-08-29 09:34:38 -0400

    load models

---
+++ axriv_researcher/load_models.py
@@ -0,0 +1,10 @@
+from pydantic_ai.models.google import GoogleModel, GoogleModelSettings
+from pydantic_ai.providers.google import GoogleProvider
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+google_provider = GoogleProvider(api_key=os.getenv('GOOGLE_API_KEY'))
+MODEL = GoogleModel('gemini-1.5-flash', provider=google_provider)
+
